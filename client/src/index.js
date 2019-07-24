d9bb1f7a6fafdf6744fb9eec658320983e462de2
Leonardo Sabino <leonardosabinoti@gmail.com>
2019-07-23 21:43:12 -0300

    Add  Gentella

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import $ from 'jquery';
-// import '../public/favicon.ico';
+import './css/custom.css';
+// import registerServiceWorker from './registerServiceWorker';
 
-const bootstrap = require('bootstrap')
+import { Left, Top, Footer } from './js/navigation';
+import history from './js/history';
+import { Router } from 'react-router';
+import PageContent, { Menu } from './js/routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import AppFactory from './js/App';
+const App = AppFactory(React, Router, history, Left, Top, Footer, PageContent, Menu);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+// registerServiceWorker();
